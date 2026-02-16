6af204453b6c209b328be558dcaec6be63ff357e
ojoedejen <dejenojoe@gmail.com>
2026-02-16 00:54:09 +0000

    Optimize dashboard for mobile UX: implement 2x2 stat grid

--- components/stat-cards.tsx
+++ components/stat-cards.tsx
@@ -109,7 +109,7 @@ export function StatCards({
         </div>
       </div>
 
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 sm:gap-5 lg:grid-cols-4">
         {cards.map((card, i) => {
           const Icon = card.icon
           return (
